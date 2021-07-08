6106df72e6682f7d9c2726cebdc52534a76a5a20
Vincent Hugues <vincenthugues.dev@gmail.com>
2021-07-08 23:50:20 +0200

    test: fix outdated tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,12 @@ import App from './App';
 describe('App', () => {
   it('renders with header', () => {
     render(<App />);
-    expect(screen.getByRole('heading')).toHaveTextContent('Facture')
+    expect(screen.getByRole("heading")).toHaveTextContent("Facturation");
   });
 
   it('renders with invoice table', () => {
     render(<App />);
-    const invoiceTableElement = screen.getByText(/Service/i);
+    const invoiceTableElement = screen.getByText(/Sélectionner une facture/i);
     expect(invoiceTableElement).toBeInTheDocument();
   });
 });
