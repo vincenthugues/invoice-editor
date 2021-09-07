ea6ea637abfb71eaa62b1df3b83a9cb9c64f5c00
Vincent Hugues <vincenthugues.dev@gmail.com>
2021-09-07 23:27:56 +0200

    test: simplify hook test

--- src/hooks.test.tsx
+++ src/hooks.test.tsx
@@ -1,32 +1,16 @@
-import { render, RenderOptions } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { usePersonalInfo } from "./hooks";
-import * as utils from './utils';
+import { getValueFromStorage, setValueInStorage } from './utils';
 
 jest.mock('./utils');
-
-let processEnv: NodeJS.ProcessEnv;
 
 describe('usePersonalInfo', () => {
   const TestComponent = () => {
     const [personalInfo, setPersonalInfo] = usePersonalInfo();
+    const { name, personalDetails, contactInfo, siret } = personalInfo || {};
 
-    return <span>Personal info: {JSON.stringify(personalInfo)}</span>;
+    return <div>{name}{personalDetails}{contactInfo}{siret}</div>;
   };
-
-  let container: Element | null;
-
-  beforeEach(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-
-    utils.getValueFromStorage = jest.fn();
-    utils.setValueInStorage = jest.fn();
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container as Element);
-    container = null;
-  });
 
   it('sets default values in storage when no value exists', () => {
     const DEFAULT_PERSONAL_INFO = {
@@ -36,26 +20,31 @@ describe('usePersonalInfo', () => {
       siret: 'SIRET',
     };
 
-    render(<TestComponent />, container as RenderOptions);
+    render(<TestComponent />);
 
-    expect(utils.getValueFromStorage).toHaveBeenCalledTimes(1);
-    expect(utils.setValueInStorage).toHaveBeenCalledTimes(1);
-    expect(utils.setValueInStorage).toHaveBeenCalledWith('personalInfo', DEFAULT_PERSONAL_INFO);
+    expect(getValueFromStorage).toHaveBeenCalledTimes(1);
+    expect(setValueInStorage).toHaveBeenCalledTimes(1);
+    expect(setValueInStorage).toHaveBeenCalledWith('personalInfo', DEFAULT_PERSONAL_INFO);
   });
 
   it('gets stored values', () => {
     const storedValues = {
-      name: 'name2',
-      personalDetails: 'personalDetails2',
-      contactInfo: 'contactInfo2',
-      siret: 'siret2',
+      name: 'TEST_NAME',
+      personalDetails: 'TEST_PERSONAL_DETAILS',
+      contactInfo: 'TEST_CONTACT_INFO',
+      siret: 'TEST_SIRET',
     };
 
-    utils.getValueFromStorage = jest.fn().mockReturnValueOnce(storedValues);
+    (getValueFromStorage as jest.Mock).mockReturnValueOnce(storedValues);
 
-    render(<TestComponent />, container as RenderOptions);
+    render(<TestComponent />);
 
-    expect(utils.getValueFromStorage).toHaveBeenCalledTimes(1);
-    expect(utils.setValueInStorage).toHaveBeenCalledTimes(0);
+    expect(getValueFromStorage).toHaveBeenCalledTimes(1);
+    expect(setValueInStorage).toHaveBeenCalledTimes(0);
+
+    expect(screen.getByText(/TEST_NAME/)).toBeInTheDocument();
+    expect(screen.getByText(/TEST_PERSONAL_DETAILS/)).toBeInTheDocument();
+    expect(screen.getByText(/TEST_CONTACT_INFO/)).toBeInTheDocument();
+    expect(screen.getByText(/TEST_SIRET/)).toBeInTheDocument();
   });
 });
